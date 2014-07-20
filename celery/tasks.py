8e51248684ea88629f09d09f5abc7011a7f96169
Vitaly <vit.nikitin@gmail.com>
2014-07-20 11:22:28 +0400

    fix headers for correct detect celery messages

--- celery/tasks.py
+++ celery/tasks.py
@@ -5,7 +5,7 @@ celery = Celery('tasks', broker='amqp://guest:guest@localhost:5672//')
 
 celery.conf.update(
     CELERY_RESULT_BACKEND="amqp",
-    CELERY_RESULT_SERIALIZER="json'
+    CELERY_RESULT_SERIALIZER="json"
 )
 
 @celery.task
